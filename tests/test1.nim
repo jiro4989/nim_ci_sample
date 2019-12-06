a2bbec7b3d9a57ce5fe18ee2947a04ffade6dde1
jiro4989 <jiroron666@gmail.com>
2019-12-06 22:17:11 +0900

    Add test code

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,12 +1,9 @@
-# This is just an example to get you started. You may wish to put all of your
-# tests into a single file, or separate them into multiple `test1`, `test2`
-# etc. files (better names are recommended, just make sure the name starts with
-# the letter 't').
-#
-# To run these tests, simply execute `nimble test`.
-
 import unittest
 
 import nim_ci_samplepkg/submodule
+
 test "correct welcome":
-  check getWelcomeMessage() == "Hello, World!"
+  check fizzbuzz(1) == "1"
+  check fizzbuzz(3) == "Fizz"
+  check fizzbuzz(5) == "Buzz"
+  check fizzbuzz(15) == "FizzBuzz"
