b034e9058d899675fc7bf42512c10abb795896d8
jiro4989 <jiroron666@gmail.com>
2019-12-06 22:00:20 +0900

    FizzBuzzを実装した

--- src/nim_ci_sample.nim
+++ src/nim_ci_sample.nim
@@ -1,7 +1,23 @@
-# This is just an example to get you started. A typical hybrid package
-# uses this file as the main entry point of the application.
-
 import nim_ci_samplepkg/submodule
+export submodule
+
+import os, strutils
+
+proc fizzbuzzLoop*(n: int) =
+  ## ``n`` 回FizzBuzzを判定して標準出力する。
+  runnableExamples:
+    fizzbuzzLoop(5)
+    ## Output:
+    ##   1
+    ##   2
+    ##   Fizz
+    ##   4
+    ##   Buzz
+  for i in 1..n:
+    echo fizzbuzz(i)
 
 when isMainModule:
-  echo(getWelcomeMessage())
+  let args = commandLineParams()
+  let n = args[0].parseInt()
+  fizzbuzzLoop(n)
+
--- src/nim_ci_samplepkg/submodule.nim
+++ src/nim_ci_samplepkg/submodule.nim
@@ -1,6 +1,14 @@
-# This is just an example to get you started. Users of your hybrid library will
-# import this file by writing ``import nim_ci_samplepkg/submodule``. Feel free to rename or
-# remove this file altogether. You may create additional modules alongside
-# this file as required.
-
-proc getWelcomeMessage*(): string = "Hello, World!"
+proc fizzbuzz*(n: int): string =
+  ## ``n`` の値を判定してFizz, Bussまたは ``n`` 自身を返す。
+  runnableExamples:
+    doAssert fizzbuzz(1) == "1"
+    doAssert fizzbuzz(3) == "Fizz"
+    doAssert fizzbuzz(5) == "Buzz"
+    doAssert fizzbuzz(15) == "FizzBuzz"
+  if n mod 15 == 0:
+    return "FizzBuzz"
+  if n mod 3 == 0:
+    return "Fizz"
+  if n mod 5 == 0:
+    return "Buzz"
+  return $n
